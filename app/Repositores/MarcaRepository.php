24b392c98dafd635e64d991ae66a19edd0a327f4
Brunimax <brunimax.bm@gmail.com>
2023-02-02 09:24:50 -0300

    Implementação de um abstractRepositorys para colocar os meetodos em comum.

--- app/Repositores/MarcaRepository.php
+++ app/Repositores/MarcaRepository.php
@@ -2,36 +2,9 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
+class MarcaRepository extends AbstractRepository {
 
-class MarcaRepository {
-
-    protected $model;
-
-    public function __construct($model) {
-        $this->model = $model;
-    }
-
-    public function selectAtributosRegistrosRelacionados($atributos) {
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro($filtros) {
-        $filtros = explode(';', $filtros);
-        
-        foreach($filtros as $key => $condicao) {
-            $condicoes = explode(':', $condicao);
-            $this->model = $this->model->where($condicoes[0], $condicoes[1], $condicoes[2]);
-        }
-    }
-
-    public function selectAtributos($atributos) {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado() {
-        return $this->model->get();
-    }
+    
 
 }
 
--- app/Repositores/ModeloRepository.php
+++ app/Repositores/ModeloRepository.php
@@ -1,37 +1,10 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Repositories;;
 
-use Illuminate\Database\Eloquent\Model;
+class ModeloRepository extends AbstractRepository {
 
-class ModeloRepository {
-
-    protected $model;
-
-    public function __construct($model) {
-        $this->model = $model;
-    }
-
-    public function selectAtributosRegistrosRelacionados($atributos) {
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro($filtros) {
-        $filtros = explode(';', $filtros);
-        
-        foreach($filtros as $key => $condicao) {
-            $condicoes = explode(':', $condicao);
-            $this->model = $this->model->where($condicoes[0], $condicoes[1], $condicoes[2]);
-        }
-    }
-
-    public function selectAtributos($atributos) {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado() {
-        return $this->model->get();
-    }
+    
 
 }
 
