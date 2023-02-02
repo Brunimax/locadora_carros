6584b7ae29f8738fec0e495ac1aec9bb3ddbaba9
Brunimax <brunimax.bm@gmail.com>
2023-02-02 09:16:59 -0300

    Implementando o repository para modelos

---
+++ app/Repositores/ModeloRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModeloRepository {
+
+    protected $model;
+
+    public function __construct($model) {
+        $this->model = $model;
+    }
+
+    public function selectAtributosRegistrosRelacionados($atributos) {
+        $this->model = $this->model->with($atributos);
+    }
+
+    public function filtro($filtros) {
+        $filtros = explode(';', $filtros);
+        
+        foreach($filtros as $key => $condicao) {
+            $condicoes = explode(':', $condicao);
+            $this->model = $this->model->where($condicoes[0], $condicoes[1], $condicoes[2]);
+        }
+    }
+
+    public function selectAtributos($atributos) {
+        $this->model = $this->model->selectRaw($atributos);
+    }
+
+    public function getResultado() {
+        return $this->model->get();
+    }
+
+}
+
+?>
